71e2567de41941478f9e5aa184910fe8defbc7b4
Fabio Weck <fabio.weck@gmail.com>
2023-08-04 16:09:48 -0600

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace EnableDisableUSB;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Management.Automation;
+
+namespace EnableDisableUSB;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Switch_Toggled(object sender, ToggledEventArgs e)
+    {
+        if (e.Value)
+        {
+            using (PowerShell ps = PowerShell.Create())
+            {
+                const string enable = @"Start-Process -FilePath ""powershell"" -windowstyle Hidden -Verb RunAs -ArgumentList 'pnputil /enable-device ""USB\VID_1235`&PID_8211\Y7R5XE21C2CFAD""'";
+                _ = ps.AddScript(enable);
+                ps.Invoke();
+            }
+        }
+        else
+        {
+            using (PowerShell ps = PowerShell.Create())
+            {
+                const string disable = @"Start-Process -FilePath ""powershell"" -windowstyle Hidden -Verb RunAs -ArgumentList 'pnputil /disable-device ""USB\VID_1235`&PID_8211\Y7R5XE21C2CFAD""'";
+                _ = ps.AddScript(disable);
+                ps.Invoke();
+            }
+        }
+    }
+}
+
